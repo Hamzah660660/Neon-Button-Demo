58119aa9fb299a78771fb871e5186671a850a443
hamzamitprogrammer <@786pak786>
2026-05-15 12:25:48 +0500

    named

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       
-      title: 'Neon Button Demo',
+      title: 'Neon Button',
        
        
       home: Scaffold(
